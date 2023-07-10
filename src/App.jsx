b5633d3829a29bd928e5e08fdf4d3a91cac0088f
drew <drewbeland@gmail.com>
2023-07-10 10:55:39 -0700

    rough header

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      {/* <Hero />
+      <Work />
+      <Contact />
+      <Footer /> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header(){
+
+    return (
+        <>
+            <header className="header">
+                <ul className="header--ul">
+                    <li className="header--li"><a className="icon" title="Linkedin" href="https://www.linkedin.com/in/drewbeland/">Linkedin</a></li>
+                    <li className="header--li"><a className="icon" title="Github" href="https://github.com/drebel">Github</a></li>
+                    <li className="header--li"><a className="icon" title="Send Email" href="mailto:drewbeland.dpt@gmail.com">Email</a></li>
+                </ul>
+            </header>
+        </>
+    )
+}
