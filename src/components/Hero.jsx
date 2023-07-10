5e30d5f3ee4c8dd367525a47bbbb676d5d2d714e
drew <drewbeland@gmail.com>
2023-07-10 11:52:25 -0700

    rough hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+
+export default function Hero(){
+
+    return (
+        <section className="hero">
+            <h1>Drew Beland</h1>
+            <h2>Physical Therapist. Software Engineer.</h2>
+            <button>View Work</button>
+            <button>View Resume</button>
+            <button>Contact Info</button>
+        </section>
+    )
+}
