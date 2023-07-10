b8535d72cc5d93d5957a30a692fe427e7bf44a5d
drew <drewbeland@gmail.com>
2023-07-10 15:01:24 -0700

    rough about

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header.jsx'
 import Hero from './components/Hero.jsx'
 import Work from './components/Work.jsx'
+import About from './components/About.jsx'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Hero />
       <Work />
+      <About />
       {/* <Hero />
       <Work />
       <Contact />
---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+
+export default function About(){
+
+    return (
+        <>
+        <section className="about">
+            <h2>About</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam iure enim vero facilis cum harum voluptatibus fugit corrupti, laborum reprehenderit repellat similique rerum mollitia perspiciatis excepturi voluptatum, nemo at necessitatibus.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptatem enim pariatur, nostrum hic qui praesentium eligendi nesciunt magnam, modi fugiat aut quae? Odit, dolor aspernatur. Architecto rerum aliquam earum!</p>
+            <button>Let's Work</button>
+        </section>
+        </>
+    )
+}
