f707374e2bc1b16775dc0e864f34e01988b0bfcc
drew <drewbeland@gmail.com>
2023-07-12 11:07:09 -0700

    mobile and desktop layout good. need to add movement to toggle function

--- src/components/Header2.jsx
+++ src/components/Header2.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react"
+
 export default function Header2(){
+
+    const [isMobileNavVisible, setIsMobileNavVisible] = useState(false)
+    const menuStyle = {
+        backgroundImage: isMobileNavVisible ? 'url(src/assets/icons8-cancel-50.png)' : 'url(src/assets/icons8-menu-50.png)'
+    }
+
+    
+    function handleClick(){
+        setIsMobileNavVisible(!isMobileNavVisible)
+    }
 
     return (
         <header className="primary-header flex" id="header">
@@ -31,7 +43,7 @@ export default function Header2(){
                 </ul>
             </nav>
 
-            <button className="mobile-nav-toggle" aria-controls="primary-navigation" aria-expanded="false">
+            <button className="mobile-nav-toggle" aria-controls="primary-navigation" aria-expanded="false" onClick={handleClick} style={menuStyle}>
                 <span className="sr-only">Menu</span>
             </button>
 
