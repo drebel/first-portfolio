bbd82f74fe8cd1d2aac8f26c7597844e73458978
drew <drewbeland@gmail.com>
2023-07-13 12:56:46 -0700

    progress on design and hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ export default function Hero(){
         <section className="hero" id="hero">
             <h1>Drew Beland</h1>
             <h2>Physical Therapist. Software Engineer.</h2>
-            <button>View Work</button>
-            <button>View Resume</button>
-            <button>Contact Info</button>
+            <a href="#contact" className="cta">
+                <span>Contact Me</span>
+            </a>
         </section>
     )
 }
