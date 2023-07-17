eece785179ea6185e2d6495b78fb7c2bd2e43fe8
drew <drewbeland@gmail.com>
2023-07-17 14:11:15 -0700

    rough work layout and design

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,13 +2,33 @@
 export default function Work(){
 
     return (
-        <section className="work" id="work">
+        <section className="work flex" id="work">
             <h2>My Work</h2>
-            <article className="work--item">
-                <img src="src\assets\gift-list-screenshot.png" alt="Gift List Screenshot" />
-                <p>A simple gift registry for your close circle of friends and family.</p>
-                <p>EJS Node.js Express Passport HTML5 CSS3 MongoDB Mongoose REST API</p>
-                <a href="https://giftlist.cyclic.app/"></a>
+            <article className="work--item flex">
+                <div className="detail">
+                    <h3 className="">Gift List</h3>
+                    <p className="description">
+                        A simple gift registry for your close circle of friends and family.
+                        <br />
+                        <a href="https://giftlist.cyclic.app/">Live Preview </a>
+                        <a href="https://github.com/drebel/gift-registry">Source Code </a>
+                        </p>
+                    <p className="tags">
+                        <span>EJS</span>
+                        <span>Node.js</span>
+                        <span>Express</span>
+                        <span>Passport</span>
+                        <span>MongoDB</span>
+                        <span>Mongoose</span>
+                        <span>HTML5</span>
+                        <span>CSS3</span>
+                        <span>Bootstrap</span>
+                        <span>REST API</span>
+                    </p>
+                </div>
+                <div className="thumbnail">
+                    <img src="src\assets\gift-list-screenshot.png" alt="Gift List Screenshot" />
+                </div>
             </article>
         </section>
     )
