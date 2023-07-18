5c2c2f5685c5f07abbf5edc4bb45b487a2fbdae8
drew <drewbeland@gmail.com>
2023-07-18 13:34:57 -0700

    rough contact layout

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -18,19 +18,24 @@ export default function Contact(){
 
     return (
         <section className="contact" id="contact">
-            <h2>Contact</h2>
-            <form>
-                <label htmlFor="form--name">Name</label>
-                <input id='form--name' type="text" placeholder="Name" onChange={handleChange}/>
-                <label htmlFor="form--email">email</label>
-                <input id="form--email" type="text" placeholder="Email" onChange={handleChange}/>
-                <label htmlFor="form--message"></label>
-                <textarea name="" id="form--message" placeholder="Message" cols="30" rows="10" onChange={handleChange}></textarea>
-                <button onClick={handleSubmit}>Send Message</button>
-            </form>
-            <div className="contact--emailArea">
+            <div className="contact--img">
+                <img src="src\assets\original-b069dfb027ddc9338205b6df5c2bd23d.jpg" alt="" />
+            </div>
+            <div className="contact--text">
+                <h2>Contact</h2>
+                <form className="form">
+                    <label htmlFor="form--name">Name</label>
+                    <input id='form--name' type="text" placeholder="Name" onChange={handleChange}/>
+                    <label htmlFor="form--email">Email</label>
+                    <input id="form--email" type="text" placeholder="Email" onChange={handleChange}/>
+                    <label htmlFor="form--message">Message</label>
+                    <textarea name="" id="form--message" placeholder="Leave a message" cols="30" rows="10" onChange={handleChange}></textarea>
+                    <button onClick={handleSubmit} className="cta">Send Message</button>
+                </form>
+            </div>
+            <div className="contact--email">
                 <h1>DrewBeland.dpt@gmail.com</h1>
-                <button className="CTA contact--emailButton">Send Mail</button>
+                <button className="cta contact--emailButton">Send Mail</button>
             </div>
         </section>
     )
